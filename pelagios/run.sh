c011235366b83bfa534749bfa3110d5f0e0107f9
Brian <bcfuchs@gmail.com>
2016-02-18 09:47:34 +0000

    cleaning up

--- pelagios/run.sh
+++ pelagios/run.sh
@@ -1,3 +1,3 @@
 python named_entity_id.py   > data/ure_pleiades_matches.csv
-python convert2n3.py "data/ure_1.tsv" "data/ure_pleiades_matches.csv" > data/uredb.n3
+python convert2n3.py "data/ure_1.tsv" "data/ure_pleiades_matches.csv" 'data/record_media.tsv' > data/uredb.n3
 python parse.py data/uredb.n3
