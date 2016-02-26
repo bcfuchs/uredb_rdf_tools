d1b491c41ac5092c4772af38d864d5f8aa39354e
Brian <bcfuchs@gmail.com>
2016-02-26 13:37:56 +0000

    first cut at parallel..busted

--- placemarker/js/pleiades_matcher.js
+++ placemarker/js/pleiades_matcher.js
@@ -2,10 +2,19 @@
 * Match Pleiades against local names
 */
 ! function() {
+    var storage,names,hasWorkers;
+
+    hasWorkers = false;
+    storage = $.localStorage;
+    names = Object.keys(pleiades_names).join('|').toLowerCase().split('|'); // loaded before this in matcher.html
+
+    if(typeof(Worker) !== "undefined") {
+	hasWorkers = true;
+    } 
     $(document).ready(function(){
 	init()
     })
-    var names = Object.keys(pleiades_names);
+    
 
     function init() {
 	$("#load-topos").click(function(){
@@ -13,8 +22,14 @@
 	    document.getElementById('topos-file').addEventListener('change', load_topos_from_csv,false);
 
 	});
+	
+	$("#load-db").click(function(){
+	    $("#db-file").toggle();
+	    document.getElementById('db-file').addEventListener('change', load_db_from_csv,false);
 
+	});
 
+	extract_names(storage.get('db'))
     }
     
     function load_topos_from_csv(e) {
@@ -43,11 +58,109 @@
 
 
     }
+/**
+*  split into tokens and check against Pleiades
+*/
+    function extract_names(data,callback) {
+	// iterate over fields
+	// get NE candidates
+	// match tri-/bigrams against pleiades file
+	// single tokens
+	var max, out,pile;
+	max = 2500;
+	out = {}
+	pile = []
+	$("#screen4 .message").html("going to process " + max + " lines...");
+	
+	var row_process = new Worker('js/process_row.js');
+	for (var i = 0; i < max; i++) {
+	    row = data[i]
+	    if (i % 100 === 0 ) {
+		$("#screen4 .message").html("processed " + i + " lines...");
+		console.log(i)
+	    }
+	    // skip rows without a-z
+
+	    if (row.join().match(/[a-zA-Z]/)) {
+		row_process.postMessage(row)
+		row_process.onmessage = function(e) {
+		    console.log(e.data)
+		    pile.push(e.data);
+		    console.log('Message received from worker');
+		    }
+		
+		//		pile.push(process_row(row))
+	    }
+	}
+	for (var i = 0; i < pile.length; i++) {
+	    var g = pile[i];
+	    for (var word in g) {
+		if (!(out[word])){
+		    out[word] = g[word]
+			
+		}
+	    }
+
+	}
+//	storage.set('db',data); // might exceed localstorage limit
+	console.log(out)
+	return out
+	
+    }
+    function process_row(row) {
+	var out = {}
+
+		for (var j = 0; j < row.length; j++) {
+		    words = row[j].split();
+		    
+		    for (var k = 0; k < words.length; k++) {
+			
+			word = words[k].replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+			if (! (out[word]) ) {
+			    var ples = check_pleiades(word);
+			    if (ples) {
+				out[word] = ples;
+			    }
+			}
+		    }
+		}
+	return out;
+
+    }
+    function load_db_from_csv(e) {
+	console.log('load_db_from_csv');
+
+	var file = e.target.files[0];
+
+	var reader = new FileReader();
+
+	reader.readAsText(file);
+
+	reader.onload = function(e) {
+
+	    var csv = e.target.result;
+
+	    var data = $.csv.toArrays(csv);
+
+	    console.log("adding csv from file...");
+	    extract_names(data);
+//	    storage.set('db',data); // might exceed localstorage limit
+	    $("#db-file").hide();
+	    console.log("done loading db file");
+	    window.location.href = "#screen3"
+	}
+
+
+
+
+
+    }
+
     
     function check_pleiades(name) {
-
-
-	if (names.indexOf(name) > -1) {
+	
+	
+	if (names.indexOf(name.toLowerCase()) > -1) {
 	    return pleiades_names[name]
 	}
 
@@ -56,7 +169,7 @@
     function load_topos_into_table(ar) {
 	console.log("load")
 
-
+	$("#place-templatebody row").remove();
 	for (var i = 0;i < ar.length; i++) {
 	    var row = $("#templaterow").clone().attr("id","name_row"+i);
 	    var namef = ar[i][0];
@@ -95,11 +208,7 @@
 			}).show();
 
 			var d = $(this).data('latlon');
-			console.log(d[0]);
-			console.log(d[1]);
-			console.log(d[2]);
-		
-			console.log("done");
+
 			// move map to left upper
    			var map = new GMaps({
 			    div: '#map',
@@ -116,7 +225,7 @@
 
 			    title: d[2],
 			    click: function(e) {
-				alert('You clicked in this marker');
+				alert(title);
 			    },
 			    infoWindow: {
 				content: '<p>'+pid+'</p>'
@@ -140,7 +249,7 @@
 
 	   
 	    
-		$("#templatebody").append(row);
+		$("#place-templatebody").append(row);
 		$(row).show();
 	    }
 	}
