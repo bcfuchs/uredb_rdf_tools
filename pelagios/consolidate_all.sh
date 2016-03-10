cf2be8b895decb1935de4ffcc9b57293be60efeb
Brian <bcfuchs@gmail.com>
2016-03-10 21:50:49 +0000

    cleaning up

--- pelagios/consolidate_all.sh
+++
@@ -1,3 +0,0 @@
-consolidate.sh
-consolidate2.sh
-#hand edited final version in place_adjectives.csv
---
+++ pelagios/make_adjective_place.sh
@@ -0,0 +1,5 @@
+# make adjective -> place name map from uredb, wikipedia and perseus data. 
+consolidate.sh
+# hand edit output for next step
+consolidate2.sh
+#hand edited final version in place_adjectives.csv
